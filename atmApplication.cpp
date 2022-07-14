5e66f95df952c75295d4853f2271849acba6d221
Annan Dejene <annandejene2020@gmail.com>
2022-07-13 18:16:58 -0700

    Added an options functions to display the options for the user

--- atmApplication.cpp
+++ atmApplication.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void options()
+{
+    cout << endl
+         << "*******MENU*******" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
+    cout << "*******************" << endl
+         << endl;
+}
+
 int main()
 {
+    int input;
+
+    options();
+    cin >> input;
 
     return 0;
 }

b564aacf5d5cfa23a3b685c858c0fd4af37d2ccb
Annan Dejene <annandejene2020@gmail.com>
2022-07-13 18:18:43 -0700

    Added name functionality

--- atmApplication.cpp
+++ atmApplication.cpp
@@ -16,10 +16,52 @@ void options()
 
 int main()
 {
-    int input;
 
-    options();
-    cin >> input;
+    int input;
+    double depositAmt, withdrawAmt;
+    double balance = 100;
+    string name;
+
+    cout << "Welcome to your ATM account" << endl;
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+
+    while (input != 4)
+    {
+
+        options();
+        cin >> input;
+
+        switch (input)
+        {
+        case 1:
+            cout << "Dear " << name << " your balance is " << balance << "ETB" << endl;
+            break;
+        case 2:
+            cout << "Enter the amount you want to deposit: ";
+            cin >> depositAmt;
+            balance += depositAmt;
+            cout << "Success!" << endl;
+            break;
+        case 3:
+            cout << "Enter the amount you want to withdraw: ";
+            cin >> withdrawAmt;
+
+            if (withdrawAmt <= balance)
+            {
+                balance -= withdrawAmt;
+                cout << "Success!" << endl;
+            }
+            else
+            {
+                cout << "Insufficient funds!" << endl;
+            }
+            break;
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
